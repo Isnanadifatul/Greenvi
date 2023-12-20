665196dc05d13adce28cbfe5c760dbdfd00d4789
Daren Muhammad <darenmuhammad10@gmail.com>
2023-12-20 11:44:01 +0700

    final API

--- server.js
+++ server.js
@@ -1,71 +1,34 @@
-'use strict';
-
-const routes = require('./src/routes');
 const Hapi = require('@hapi/hapi');
-const inert = require('@hapi/inert');
-const path = require('path');
-const Boom = require('@hapi/boom');
-const connection = require('./db-config');
-const { loginHandler } = require('./src/handlers-login');
-
-
+const routes = require('./src/routes');
 
 const init = async () => {
-
-    const server = Hapi.Server({
-        host: 'localhost',
-        port: 5002,
-        routes: {
-            cors: {
-                origin: ['*'],
-            }
-        },
-    });
-
-    server.state('userSession', {
-        ttl: 24 * 60 * 60 * 1000,
-        encoding: 'base64json',
-        isSecure: false,
-        isHttpOnly: true,
-        clearInvalid: false,
-        strictHeader: true,
-    });
-
-    await server.register([{
-        plugin: require("hapi-geo-locate"),
-        options: {
-            enabledByDefault: true
-        }
+  const server = Hapi.server({
+    host: 'localhost',
+    port: process.env.PORT || 5000,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
     },
-    {
-        plugin: inert
-    },
-    {
-        plugin: require('@hapi/vision')
-    },
-    {
-        plugin: require('@hapi/cookie')
-    },
-    ]);
+  });
 
-    // Konfigurasi rendering view dengan handlebars (contoh)
-    server.views({
-        engines: {
-            html: require('handlebars'),
-        },
-        relativeTo: __dirname,
-        path: 'views', // Pastikan ini sesuai dengan struktur folder proyek Anda
-    });
+  server.state('userSession', {
+    ttl: 24 * 60 * 60 * 1000,
+    encoding: 'base64json',
+    isSecure: false,
+    isHttpOnly: true,
+    clearInvalid: false,
+    strictHeader: true,
+  });
 
-    server.route(routes);
+  server.route(routes);
 
-    await server.start();
-    console.log(`Server started on: ${server.info.uri}`);
+  await server.register({
+    plugin: require('@hapi/cookie'),
+  });
+
+  await server.start();
+  console.log(`Server started on ${server.info.uri}`);
 };
-
-process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
-});
 
 init();
